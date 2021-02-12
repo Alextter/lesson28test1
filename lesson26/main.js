a36dad7d488334840f4b0985d396c529047e9063
Alextter <74066029+Alextter@users.noreply.github.com>
2021-02-12 18:52:31 +0200

    Add files via upload

---
+++ lesson26/main.js
@@ -0,0 +1,35 @@
+
+
+let x = prompt("Добрый день!")
+let y = prompt("Как вас зовут?")
+let c = prompt(y + " на какую должность вы расчитываете?")
+let r = prompt("Каким опытом и навыками вы обладете в этой сфере?")
+let t = prompt("Есть ли у вас вредные привычки?")
+let g = prompt(" Как вы думаете почему мы должны взять именно вас?")
+let l = prompt("Если мы вас наймём каковы гарантии того что вы не уволитесь через месяц?")
+let o = prompt("На какую ставку вы расчитываете в переспективе развития?")
+let p = prompt("Насколько вы стрэсоустойчивы")
+let h = prompt("Есть ли у вас рекомендации от роботодателей?")
+let j = prompt("Вы конфликтны в жизни?")
+let i = prompt("Спасибо, мы подумали и вы приняты на испытательный срок длительностью в месяц!")
+let b = prompt("Ваш робочий день начинается завтра с восьми часов!")
+
+
+
+
+
+
+console.log(x)
+console.log(y)
+console.log(c)
+console.log(r)
+console.log(t)
+console.log(g)
+console.log(l)
+console.log(o)
+console.log(p)
+console.log(h)
+console.log(j)
+console.log(i)
+console.log(b)
+
